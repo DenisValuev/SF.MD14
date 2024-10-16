52bb65b3927e6e78d6290b5535a9dad78b2ffb67
DenisValuev <valuev.denis91@mail.ru>
2024-10-16 15:58:09 +0300

    Добавьте файлы проекта.

---
+++ SF.MD14.U0/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SF.MD14.U0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task4
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян", "Аккакий" };
+
+            //Список, куда будем сохранять выборку
+            var orderedList = new List<string>();
+
+            //Пробежимся по массиву и добавим искомое в наш список
+            foreach (var person in people)
+            {
+                if (person.ToUpper().StartsWith("А"))
+                {
+                    orderedList.Add(person);
+                }
+            }
+
+            //Отсортируем список
+            orderedList.Sort();
+            foreach (string s in orderedList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
